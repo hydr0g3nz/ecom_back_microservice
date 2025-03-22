a3385ed160f3893df547c95dcf78ba40389ccb3a
Hydr0g3n <renbowneos@gmail.com>
2025-03-23 05:36:45 +0700

    Add Zap implementation for structured logging

--- pkg/logger/zap_logger.go
+++ pkg/logger/zap_logger.go
@@ -2,31 +2,37 @@ package logger
 
 import (
 	"os"
+	"strings"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-// Logger defines the interface for logging
-type Logger interface {
-	Debug(msg string, keysAndValues ...interface{})
-	Info(msg string, keysAndValues ...interface{})
-	Warn(msg string, keysAndValues ...interface{})
-	Error(msg string, keysAndValues ...interface{})
-	Fatal(msg string, keysAndValues ...interface{})
-}
-
-// zapLogger implements the Logger interface using zap
-type zapLogger struct {
+// ZapLogger implements the Logger interface using Zap
+type ZapLogger struct {
 	logger *zap.SugaredLogger
 }
 
-// NewZapLogger creates a new instance of Logger using zap
-func NewZapLogger() Logger {
-	// Determine log level from environment
+// NewZapLogger creates a new instance of ZapLogger
+func NewZapLogger() *ZapLogger {
+	// Create a production encoder
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	
+	// Determine if we should use JSON or console format
+	var encoder zapcore.Encoder
+	logFormat := os.Getenv("LOG_FORMAT")
+	if strings.ToLower(logFormat) == "console" {
+		encoder = zapcore.NewConsoleEncoder(encoderConfig)
+	} else {
+		encoder = zapcore.NewJSONEncoder(encoderConfig)
+	}
+	
+	// Set log level based on environment
+	level := zapcore.InfoLevel
 	logLevel := os.Getenv("LOG_LEVEL")
-	var level zapcore.Level
-	switch logLevel {
+	switch strings.ToLower(logLevel) {
 	case "debug":
 		level = zapcore.DebugLevel
 	case "info":
@@ -35,61 +41,57 @@ func NewZapLogger() Logger {
 		level = zapcore.WarnLevel
 	case "error":
 		level = zapcore.ErrorLevel
-	default:
-		level = zapcore.InfoLevel
 	}
-
-	// Create encoder config
-	encoderConfig := zapcore.EncoderConfig{
-		TimeKey:        "time",
-		LevelKey:       "level",
-		NameKey:        "logger",
-		CallerKey:      "caller",
-		FunctionKey:    zapcore.OmitKey,
-		MessageKey:     "msg",
-		StacktraceKey:  "stacktrace",
-		LineEnding:     zapcore.DefaultLineEnding,
-		EncodeLevel:    zapcore.LowercaseLevelEncoder,
-		EncodeTime:     zapcore.ISO8601TimeEncoder,
-		EncodeDuration: zapcore.SecondsDurationEncoder,
-		EncodeCaller:   zapcore.ShortCallerEncoder,
-	}
-
-	// Create core
+	
+	// Create a core with stdout
 	core := zapcore.NewCore(
-		zapcore.NewJSONEncoder(encoderConfig),
+		encoder,
 		zapcore.AddSync(os.Stdout),
-		level,
+		zap.NewAtomicLevelAt(level),
 	)
-
-	// Create logger
-	logger := zap.New(core, zap.AddCaller(), zap.AddCallerSkip(1))
-	return &zapLogger{
-		logger: logger.Sugar(),
-	}
+	
+	// Create the logger
+	logger := zap.New(
+		core,
+		zap.AddCaller(),
+		zap.AddCallerSkip(1),
+		zap.AddStacktrace(zapcore.ErrorLevel),
+	).Sugar()
+	
+	return &ZapLogger{logger: logger}
 }
 
-// Debug logs a message at debug level
-func (l *zapLogger) Debug(msg string, keysAndValues ...interface{}) {
+// Debug logs a debug message
+func (l *ZapLogger) Debug(msg string, keysAndValues ...interface{}) {
 	l.logger.Debugw(msg, keysAndValues...)
 }
 
-// Info logs a message at info level
-func (l *zapLogger) Info(msg string, keysAndValues ...interface{}) {
+// Info logs an info message
+func (l *ZapLogger) Info(msg string, keysAndValues ...interface{}) {
 	l.logger.Infow(msg, keysAndValues...)
 }
 
-// Warn logs a message at warn level
-func (l *zapLogger) Warn(msg string, keysAndValues ...interface{}) {
+// Warn logs a warning message
+func (l *ZapLogger) Warn(msg string, keysAndValues ...interface{}) {
 	l.logger.Warnw(msg, keysAndValues...)
 }
 
-// Error logs a message at error level
-func (l *zapLogger) Error(msg string, keysAndValues ...interface{}) {
+// Error logs an error message
+func (l *ZapLogger) Error(msg string, keysAndValues ...interface{}) {
 	l.logger.Errorw(msg, keysAndValues...)
 }
 
-// Fatal logs a message at fatal level and then exits
-func (l *zapLogger) Fatal(msg string, keysAndValues ...interface{}) {
+// Fatal logs a fatal message and then calls os.Exit(1)
+func (l *ZapLogger) Fatal(msg string, keysAndValues ...interface{}) {
 	l.logger.Fatalw(msg, keysAndValues...)
+}
+
+// With returns a logger with the specified key-value pairs
+func (l *ZapLogger) With(keysAndValues ...interface{}) Logger {
+	return &ZapLogger{logger: l.logger.With(keysAndValues...)}
+}
+
+// WithCorrelationID returns a logger with the correlation ID field
+func (l *ZapLogger) WithCorrelationID(correlationID string) Logger {
+	return &ZapLogger{logger: l.logger.With("correlation_id", correlationID)}
 }
